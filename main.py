2f8260b30ac5331ada443bb1d0af48ae7c7f57af
scott <scott@news123.tw>
2022-04-05 17:31:00 +0800

    add csv split files

--- main.py
+++ main.py
@@ -3,19 +3,31 @@ from jp_tokenizer.core import get_i_adjectives, get_na_adjectives, get_verbs
 
 
 if __name__ == '__main__':
-    words = list()
+    words = dict()
     content = read_words_from_csv('data/vocabulary_65001.csv')
     for line in content:
-        words.append(line[0])
+        words[line[0]] = line
 
     adj_list = get_i_adjectives(words)
     print(adj_list)
     print(len(adj_list))
+    with open('data/all_i_adj.csv', 'w', encoding='utf8') as f:
+        for word in adj_list:
+            line = ','.join(words[word])
+            f.write(line)
 
     adj_list = get_na_adjectives(words)
     print(adj_list)
     print(len(adj_list))
+    with open('data/all_na_adj.csv', 'w', encoding='utf8') as f:
+        for word in adj_list:
+            line = ','.join(words[word])
+            f.write(line)
 
     verb_list = get_verbs(words)
     print(verb_list)
     print(len(verb_list))
+    with open('data/all_verbs.csv', 'w', encoding='utf8') as f:
+        for word in verb_list:
+            line = ','.join(words[word])
+            f.write(line)
